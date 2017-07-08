6bab91e55899d7bb8ff2f6c05a6187cb60216a63
Jon Ho <jonwho.contact@gmail.com>
2017-07-08 14:54:57 -0700

    more refs

--- app/models/contract.rb
+++ app/models/contract.rb
@@ -1,2 +1,4 @@
 class Contract < ApplicationRecord
+  belongs_to :contractor, class_name: 'User', foreign_key: 'user_id'
+  belongs_to :contractee, class_name: 'User', foreign_key: 'user_id'
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :ads
+  has_many :contracts
+  has_many :contractees, through: :contracts
+  has_many :contractors, through: :contracts
 end
