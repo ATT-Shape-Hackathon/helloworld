e4b654ef9e3457c4dde54e1a88c1f14d52c89157
Jon Ho <jonwho.contact@gmail.com>
2017-07-08 14:19:58 -0700

    create ads table

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception, prepend: true
+
+  protected
+
+  def authenticate_user!
+    if user_signed_in?
+      super
+    else
+      redirect_to login_path, notice: 'Must login first'
+    end
+  end
 end
